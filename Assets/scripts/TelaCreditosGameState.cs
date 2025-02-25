3a81db679a318df9fded03acab22fbafb777cc2d
Mauricio Braga <braga.mauricio@gmail.com>
2025-02-25 11:34:11 -0300

    Adicionada mudança de estados com espaço.

--- Assets/scripts/TelaCreditosGameState.cs
+++ Assets/scripts/TelaCreditosGameState.cs
@@ -1,15 +1,24 @@
 using UnityEngine;
 
-public class TelaCreditosGameState : GameBaseState    {
-    public override void enterState(GameStateManager gameState)  {
+public class TelaCreditosGameState : GameBaseState
+{
+    public override void enterState(GameStateManager gameState)
+    {
         Debug.Log("Entramos na Tela creditos.");
     }
 
-    public override void updateState(GameStateManager gameState)   {
-        
+    public override void updateState(GameStateManager gameState)
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // muda para o próximo estado.
+            gameState.switchState(gameState.telaInicialState);
+        }
+
     }
 
-    public override void leaveState(GameStateManager gameState)  {
+    public override void leaveState(GameStateManager gameState)
+    {
         Debug.Log("Saindo da Tela creditos.");
     }
 }
--- Assets/scripts/TelaInicialGameState.cs
+++ Assets/scripts/TelaInicialGameState.cs
@@ -1,15 +1,24 @@
 using UnityEngine;
 
-public class TelaInicialGameState : GameBaseState    {
-    public override void enterState(GameStateManager gameState)  {
+public class TelaInicialGameState : GameBaseState
+{
+    public override void enterState(GameStateManager gameState)
+    {
         Debug.Log("Entramos na Tela inicial.");
     }
 
-    public override void updateState(GameStateManager gameState)   {
-        
+    public override void updateState(GameStateManager gameState)
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // muda para o próximo estado.
+            gameState.switchState(gameState.telaCreditosState);
+        }
+
     }
 
-    public override void leaveState(GameStateManager gameState)  {
+    public override void leaveState(GameStateManager gameState)
+    {
         Debug.Log("Saindo da Tela inicial.");
     }
 }
